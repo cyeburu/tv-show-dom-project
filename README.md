31f00250d539b46a2468a3fb6b3f9da01e7e59a7
Cynthia Eburu <cyeburu@yahoo.com>
2020-04-25 16:38:54 +0100

    tv-episode-100L

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 A starting point for CYF's TV show DOM project
 
 The requirements for the project are here:
+//https://github.com/CodeYourFuture/syllabus/tree/london/js-core-3/tv-show-dom-project
 
 https://github.com/CodeYourFuture/syllabus/tree/master/js-core-3/tv-show-dom-project/readme.md
--- script.js
+++ script.js
@@ -4,9 +4,42 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+//console.log(document.body.style.background = "grey");
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`
 
+  for (let i = 0; i < episodeList.length; i++) {
+
+    let episodediv = document.createElement("div");
+    rootElem.appendChild(episodediv);
+    episodediv.className = "episodeDiv";
+
+    let episodeName = document.createElement("h1");
+    let episodeImage = document.createElement("img");
+    episodeImage.src = episodeList[i].image.medium
+    let episodeSummaryText = document.createElement("p")
+
+    episodediv.appendChild(episodeName)
+    episodediv.appendChild(episodeImage)
+    episodediv.appendChild(episodeSummaryText)
+
+    let seasonNumber = episodeList[i].season.toString().padStart(2, "0")
+    console.log(seasonNumber);
+    let episodeNumber = episodeList[i].number.toString().padStart(2, "0")
+    console.log(episodeNumber);
+    let episodeCode = `S${seasonNumber}E${episodeNumber}`
+    console.log(episodeCode);
+
+    episodeName.innerHTML = episodeList[i].name + " " + "- " + episodeCode;
+    console.log(episodeName);
+
+    episodeImage.innerHTML = episodeList[i].image.medium;
+    console.log(episodeImage);
+
+    episodeSummaryText.innerHTML = episodeList[i].summary
+    console.log(episodeSummaryText)
+  }
+}
 window.onload = setup;
