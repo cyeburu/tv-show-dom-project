c64ecf2c4d8917a2681ac452c19af82d410b0b87
Cynthia Eburu <cyeburu@yahoo.com>
2020-05-15 18:10:35 +0100

    change selectBar

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
 function setup() {
   const allShows = getAllShows();
   makePageForShows(allShows);
   const allEpisodes = getAllEpisodes();
   console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
-  matchEpisodeValue(allEpisodes);
 }
-
 let rootElem;
 let searchBar;
 let episodeName;
@@ -15,12 +12,8 @@ function makePageForShows(shows) {
   searchBar = document.createElement("div")
   document.body.appendChild(searchBar);
   document.body.insertBefore(searchBar, document.body.childNodes[0]);
-
   let searchInput = document.createElement("input");
   searchBar.appendChild(searchInput)
-  searchInput.type = 'search';
-  searchInput.placeholder = "Live Search";
-  rootElem = document.getElementById("root");
 
   for (let i = 0; i < shows.length; i++) {
     let episodediv = document.createElement("div");
@@ -29,7 +22,6 @@ function makePageForShows(shows) {
     let episodeImage = document.createElement("img");
     episodeImage.src = shows[i].image.medium
     episodeSummaryText = document.createElement("p")
-
     //append children to parent element
     rootElem.appendChild(episodediv);
     episodediv.appendChild(episodeName);
@@ -59,8 +51,25 @@ function makePageForShows(shows) {
     let name = shows[i].name;
     let showOption = document.createElement("option");
     selectShowBar.appendChild(showOption);
+    console.log(selectShowBar);
     showOption.innerText = name;
   }
+  selectShowBar.addEventListener("change", selectShowBarFunciton)
+}
+//get select episode in the other select bar
+function selectShowBarFunciton(event) {
+  let userSel = event.target.value;
+  console.log(userSel)
+  let selectEpisode = document.querySelectorAll(".select2")[0];
+  let selectShow = document.querySelector(".select");
+  selectShow.forEach(element => {
+    let innerHTMLContent = element.innerHTML
+    if (innerHTMLContent.indexOf(selectEpisode) != -1) {
+      element.style.display = 'block'
+    } else {
+      element.style.display = 'none'
+    }
+  })
 }
 function makePageForEpisodes(episodeList) {
   let selectEpisodeBar = document.createElement("select");
@@ -71,17 +80,21 @@ function makePageForEpisodes(episodeList) {
     let episodeNumber = episodeList[i].number.toString().padStart(2, "0")
     let episodeCode = `S${seasonNumber}E${episodeNumber}`
     let name = episodeList[i].name + " " + "- " + episodeCode;
-    //let epicontent = name.split("-").reverse().join("");
+    let epicontent = name.split("-").reverse().join("");
+    console.log(epicontent);
     let episodeOption = document.createElement("option");
     selectEpisodeBar.appendChild(episodeOption);
     episodeOption.innerText = name;
   }
   selectEpisodeBar.addEventListener("change", displaySelect)
   function displaySelect(selected) {
+    console.log("hurray!")
     let userSelect = selected.target.value;
     let selectList = Array.from(document.querySelectorAll(".episodeDiv"))
     selectList.forEach(element => {
       let innerHTMLContent = element.innerHTML
+      console.log(innerHTMLContent);
+      console.log(userSelect);
       if (innerHTMLContent.indexOf(userSelect) != -1) {
         element.style.display = 'block';
       } else {
@@ -91,5 +104,3 @@ function makePageForEpisodes(episodeList) {
   }
 }
 window.onload = setup;
-
-
